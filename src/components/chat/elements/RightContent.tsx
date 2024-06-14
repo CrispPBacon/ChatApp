3648ea3b02e92d7e202c61f3877c6196abbb009e
Allan Soriano <alsoriano@letran-manaoag.edu.ph>
2024-06-14 16:47:53 +0800

    update

--- src/components/chat/elements/RightContent.tsx
+++ src/components/chat/elements/RightContent.tsx
@@ -1,5 +1,5 @@
 import { imagesType } from "../../../interfaces";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useChat } from "../../../context/ChatContext";
 
 interface RightContentProps {
@@ -14,6 +14,8 @@ export default function RightContent({
   fetchImages,
 }: RightContentProps) {
   const { chat } = useChat();
+  const imageRefs = useRef<HTMLDivElement[]>([]);
+  const [observer, setObserver] = useState<IntersectionObserver | null>(null);
 
   useEffect(() => {
     if (chat?._id) {
@@ -21,32 +23,62 @@ export default function RightContent({
     }
   }, [chat]);
 
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5, // Trigger when 50% of the image is visible
+    };
+
+    const newObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const target = entry.target as HTMLDivElement;
+          const imageSrc = target.getAttribute("data-src");
+          if (imageSrc) {
+            target.innerHTML = `<img src="${imageSrc}" alt="image" />`;
+            newObserver.unobserve(target);
+          }
+        }
+      });
+    }, options);
+
+    setObserver(newObserver);
+
+    return () => {
+      if (newObserver) {
+        newObserver.disconnect();
+      }
+    };
+  }, [images]);
+
+  useEffect(() => {
+    if (observer && imageRefs.current.length > 0) {
+      imageRefs.current.forEach((imageRef) => {
+        observer.observe(imageRef);
+      });
+    }
+  }, [observer, images]);
+
   return (
     <div className="content">
       <div className="media">
         {images.length > 0
           ? images.map((image: imagesType, index: number) => (
-              <Image
+              <div
                 key={index}
-                src={image.imageData}
+                className="image-box"
+                ref={(element) => {
+                  if (element) {
+                    imageRefs.current[index] = element;
+                  }
+                }}
+                data-src={image.imageData}
                 onClick={() => setImage(image)}
               />
             ))
           : null}
       </div>
-    </div>
-  );
-}
-
-interface ImageProps {
-  src: string;
-  onClick: () => void;
-}
-
-function Image({ src, onClick }: ImageProps) {
-  return (
-    <div className="image-box" onClick={onClick}>
-      <img src={src} alt="image" />
     </div>
   );
 }
