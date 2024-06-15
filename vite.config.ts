2a6585f1eaf04e48686955b63853ce6b630ad7eb
Allan Soriano <alsoriano@letran-manaoag.edu.ph>
2024-06-15 17:25:26 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3500",
+        target: "https://chatappserver-rwzn.onrender.com",
         changeOrigin: true,
         secure: true,
       },
